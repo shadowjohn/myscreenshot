77d2b2bbaf6af81d02b4430030576b09a92c30e4
John <john@gis.tw>
2020-12-30 11:48:59 +0800

    初次簽入

---
+++ myscreenshot/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace myscreenshot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.wB = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(223, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "抓圖";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // wB
+            // 
+            this.wB.Location = new System.Drawing.Point(5, 54);
+            this.wB.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wB.Name = "wB";
+            this.wB.Size = new System.Drawing.Size(516, 333);
+            this.wB.TabIndex = 1;
+            this.wB.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.wB_Navigated);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(472, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(428, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "https://3wa.tw";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.wB);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "神奇的抓圖機V0.1";
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser wB;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ myscreenshot/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using utility;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+namespace myscreenshot
+{
+    public partial class Form1 : Form
+    {
+        myinclude my = new myinclude();
+        string PWD = "";
+        string OUTPUT_PATH = "";
+        //Making driver to navigate
+
+
+        static FirefoxDriver driver = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string URL = textBox1.Text;
+            if (URL.Length < 8 || URL.Substring(0, 4) != "http")
+            {
+                MessageBox.Show("網址錯了...");
+                return;
+            }
+            wB.Navigate(URL);
+        }
+
+        private void wB_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            textBox1.Text = wB.Url.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "抓圖中...";
+            button1.Enabled = false;
+
+            new Thread(() =>
+            {
+                try
+                {
+                    var driverService = FirefoxDriverService.CreateDefaultService();
+                    driverService.HideCommandPromptWindow = true; //hide console window
+                    var driverOptions = new FirefoxOptions();
+                    driverOptions.AddArguments("--headless");
+                    driverOptions.AddArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/66.0");
+                    driver = new OpenQA.Selenium.Firefox.FirefoxDriver(driverService, driverOptions);
+
+                    string URL = textBox1.Text;
+                    driver.Navigate().GoToUrl(URL);
+                    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+                    Image ig = GetEntireScreenshot();
+
+                    //Save the screenshot
+                    string t = my.date("YmdHis") + ".png";
+                    string filePath = OUTPUT_PATH + "\\" + t;
+                    ig.Save(filePath, ImageFormat.Png);
+                    string argument = "/select, \"" + filePath + "\"";
+                    System.Diagnostics.Process.Start("explorer.exe", argument);
+                    driver.Close();
+                    button1.Text = "抓圖";
+                    UpdateUIEnable(button1, true);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message + "\r\n" + ex.StackTrace);
+                    button1.Text = "抓圖";
+                    UpdateUIEnable(button1, true);
+                }
+            }).Start();
+        }
+        private delegate void UpdateUIEnableCallBack(Control ctl, bool value);
+        private void UpdateUIEnable(Control ctl, bool value)
+        {
+            if (this.InvokeRequired)
+            {
+                UpdateUIEnableCallBack uu = new UpdateUIEnableCallBack(UpdateUIEnable);
+                this.Invoke(uu, ctl, value);
+            }
+            else
+            {
+                ctl.Enabled = value;
+            }
+        }
+        public Image GetEntireScreenshot()
+        {
+            // Get the total size of the page
+            var totalWidth = (int)(long)((IJavaScriptExecutor)driver).ExecuteScript("return document.body.offsetWidth"); //documentElement.scrollWidth");
+            var totalHeight = (int)(long)((IJavaScriptExecutor)driver).ExecuteScript("return  document.body.parentNode.scrollHeight");
+            // Get the size of the viewport
+            var viewportWidth = (int)(long)((IJavaScriptExecutor)driver).ExecuteScript("return document.body.clientWidth"); //documentElement.scrollWidth");
+            var viewportHeight = (int)(long)((IJavaScriptExecutor)driver).ExecuteScript("return window.innerHeight"); //documentElement.scrollWidth");
+
+            // We only care about taking multiple images together if it doesn't already fit
+            if (totalWidth <= viewportWidth && totalHeight <= viewportHeight)
+            {
+                var screenshot = driver.GetScreenshot();
+                return ScreenshotToImage(screenshot);
+            }
+            // Split the screen in multiple Rectangles
+            var rectangles = new List<Rectangle>();
+            // Loop until the totalHeight is reached
+            for (var y = 0; y < totalHeight; y += viewportHeight)
+            {
+                var newHeight = viewportHeight;
+                // Fix if the height of the element is too big
+                if (y + viewportHeight > totalHeight)
+                {
+                    newHeight = totalHeight - y;
+                }
+                // Loop until the totalWidth is reached
+                for (var x = 0; x < totalWidth; x += viewportWidth)
+                {
+                    var newWidth = viewportWidth;
+                    // Fix if the Width of the Element is too big
+                    if (x + viewportWidth > totalWidth)
+                    {
+                        newWidth = totalWidth - x;
+                    }
+                    // Create and add the Rectangle
+                    var currRect = new Rectangle(x, y, newWidth, newHeight);
+                    rectangles.Add(currRect);
+                }
+            }
+            // Build the Image
+            var stitchedImage = new Bitmap(totalWidth, totalHeight);
+            // Get all Screenshots and stitch them together
+            var previous = Rectangle.Empty;
+            foreach (var rectangle in rectangles)
+            {
+                // Calculate the scrolling (if needed)
+                if (previous != Rectangle.Empty)
+                {
+                    var xDiff = rectangle.Right - previous.Right;
+                    var yDiff = rectangle.Bottom - previous.Bottom;
+                    // Scroll
+                    ((IJavaScriptExecutor)driver).ExecuteScript(String.Format("window.scrollBy({0}, {1})", xDiff, yDiff));
+                }
+                // Take Screenshot
+                var screenshot = driver.GetScreenshot();
+                // Build an Image out of the Screenshot
+                var screenshotImage = ScreenshotToImage(screenshot);
+                // Calculate the source Rectangle
+                var sourceRectangle = new Rectangle(viewportWidth - rectangle.Width, viewportHeight - rectangle.Height, rectangle.Width, rectangle.Height);
+                // Copy the Image
+                using (var graphics = Graphics.FromImage(stitchedImage))
+                {
+                    graphics.DrawImage(screenshotImage, rectangle, sourceRectangle, GraphicsUnit.Pixel);
+                }
+                // Set the Previous Rectangle
+                previous = rectangle;
+            }
+            return stitchedImage;
+        }
+
+        private Image ScreenshotToImage(Screenshot screenshot)
+        {
+            Image screenshotImage;
+            using (var memStream = new MemoryStream(screenshot.AsByteArray))
+            {
+                screenshotImage = Image.FromStream(memStream);
+            }
+            return screenshotImage;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PWD = my.pwd();
+            OUTPUT_PATH = PWD + "\\output";
+            Console.WriteLine(OUTPUT_PATH);
+            if (!my.is_dir(OUTPUT_PATH))
+            {
+                my.mkdir(OUTPUT_PATH);
+            }
+            button2_Click(sender, e);
+
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            driver.Dispose();
+        }
+    }
+}
